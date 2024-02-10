942bdb69c66899058aaaf7b6d9fc85267477b3b5
gvs2025 <gvs2025@gmail.com>
2024-02-10 16:02:28 -0500

    hello-world project

---
+++ micro-services-practice/hello-world/src/main/java/com/zeal2learn/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.zeal2learn.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
